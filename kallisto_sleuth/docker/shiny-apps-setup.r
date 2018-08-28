357209ad2fdbb67150d3e954573d5b84ec6e730c
Jorge Perez de Acha <perezde@cshl.edu>
2018-08-28 15:10:41 -0400

    Removed keys

--- kallisto_sleuth/docker/shiny-apps-setup.r
+++ kallisto_sleuth/docker/shiny-apps-setup.r
@@ -2,4 +2,4 @@
 
 library(rsconnect)
 
-rsconnect::setAccountInfo(name='dnalc', token='DB95E3A9AC286BF1D0171DE3FA47BBF0', secret='FtSZTw6unWQIhfvjFFmTCvWEdncz7wqzlXwvqZaP')
+rsconnect::setAccountInfo(name='', token='', secret='')
